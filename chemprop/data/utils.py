85cb44ff67a172757c516013a2f7cd72d7ca6b2f
Guy Rosin <guy@mana.bio>
2023-09-13 15:47:51 +0300

    Minor logging improvement

--- chemprop/data/utils.py
+++ chemprop/data/utils.py
@@ -485,9 +485,9 @@ def get_data(path: str,
         fieldnames = schema.names
 
     if any([c not in fieldnames for c in smiles_columns]):
-        raise ValueError(f'Data file did not contain all provided smiles columns: {smiles_columns}. Data file field names are: {fieldnames}')
+        raise ValueError(f'Data file did not contain all provided smiles columns: {smiles_columns}. Data file field names are: {fieldnames}. Missing smiles columns: {[c for c in smiles_columns if c not in fieldnames]}')
     if any([c not in fieldnames for c in target_columns]):
-        raise ValueError(f'Data file did not contain all provided target columns: {target_columns}. Data file field names are: {fieldnames}')
+        raise ValueError(f'Data file did not contain all provided target columns: {target_columns}. Data file field names are: {fieldnames}. Missing target columns: {[c for c in target_columns if c not in fieldnames]}')
 
     all_smiles, all_targets, all_atom_targets, all_bond_targets, all_rows, all_features, all_phase_features, all_constraints_data, all_raw_constraints_data, all_weights, all_gt, all_lt = [], [], [], [], [], [], [], [], [], [], [], []
     for i, row in enumeration:
